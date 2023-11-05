b3dffca298f52888d75887bdb99111d90ffa7856
Guilherme Daniel <gui.dani13@gmail.com>
2023-11-05 15:31:13 -0300

    update hero

--- UI/Hero.tsx
+++ UI/Hero.tsx
@@ -12,21 +12,32 @@ export default async function Hero() {
         backgroundAttachment: "fixed",
       }}
     >
-      {/* <div className="hero-overlay"></div> */}
-      <div className="hero-content text-center w-full bg-slate-900 bg-opacity-80 text-white rounded-md">
-        <div className=" w-full">
-          <h1 className="mb-5 text-5xl font-bold">LIMS</h1>
-          <p className="mb-5 text-2xl font-bold">
-            Torne-se o estímulo para a inovação tecnológica.
+      <div className="hero-overlay"></div>
+      <div className="container mx-auto">
+        <div className="px-4 w-full text-white flex items-center justify-center flex-col md:max-w-4xl">
+          <h1 className="mb-5 text-5xl font-bold">
+            Laboratório de Inovações em Sistemas Multimídias
+          </h1>
+          <p className="mb-5 text-2xl ">
+            Nossa missão é criar um mundo mais confortável através do
+            conhecimento tecnológico e computacional.
           </p>
-          <p className="mb-5 text-2xl font-bold">
-            Seja co-criador da nova geração de soluções tecnológicas.
-          </p>
-          <p className="mb-5 text-2xl font-bold">
-            Dê início à revolução com suas contribuições e ideias.
-          </p>
+          <div className="flex gap-4 items-start justify-start w-full">
+            <button className="btn btn-primary">Projetos</button>
+            <button className="btn btn-primary">Eventos</button>
+          </div>
         </div>
       </div>
     </div>
   );
 }
+
+// .hero-content {
+//   z-index: 0;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   max-width: 80rem/* 1280px */;
+//   gap: 1rem/* 16px */;
+//   padding: 1rem/* 16px */;
+// }
--- UI/SobreNos.tsx
+++ UI/SobreNos.tsx
@@ -16,18 +16,18 @@ export default function SobreNos() {
         <div>
           <h1 className="text-5xl font-bold">Sobre nós!</h1>
           <p className="py-6">
-            Laboratorio de Inovações em Sistemas Multimidias ou LIMS, é um
+            Laboratorio de Inovações em Sistemas Multimídias ou LIMS, é um
             laboratório de ideias inovadoras para o mundo, que utiliza uma
             grande variedade de recursos e tecnologias. A sua missão é criar um
             mundo mais confortável através do conhecimento tecnológico e
-            computacional, atendendo a diversas propostas. .
+            computacional, atendendo a diversas propostas.
           </p>
           <p className="py-6">
             O LIMS é um grupo de pesquisa que atua desde 2006 no Instituto
             Federal do Piauí (IFPI), no Campus Teresina Central. Reunindo
             pesquisadores, professores, alunos e técnicos de diferentes
             instituições de ensino e níveis de formação, que desenvolvem
-            projetos nas áreas de comunicação, tecnologia e educação..
+            projetos nas áreas de comunicação, tecnologia e educação.
           </p>
         </div>
       </div>
